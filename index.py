211372e67d948f6c5c64a79fa3ab4d6eef109e4d
Pablo Ingino <pingino@interbanking.com.ar>
2021-05-05 15:45:15 -0300

    push

--- index.py
+++ index.py
@@ -12,11 +12,13 @@ client = hazelcast.HazelcastClient(
 # Get the Distributed Map from Cluster.
 my_map = client.get_map("map-testing").blocking()
 # Standard Put and Get
-my_map.put("key", "value")
+my_map.put("key2", "value2")
+my_map.put("key3", "value3")
+my_map.put("client_id", "88223")
 my_map.get("key")
 # Concurrent Map methods, optimistic updating
-my_map.put_if_absent("somekey", "somevalue")
-my_map.replace_if_same("key", "value", "newvalue")
+#my_map.put_if_absent("somekey", "somevalue")
+#my_map.replace_if_same("key", "value", "newvalue")
 
 
 client.shutdown()
