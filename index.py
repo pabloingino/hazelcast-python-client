5e2fb581871a36662dceb35b2275ce55d95426e8
Pablo Ingino <pingino@interbanking.com.ar>
2021-05-05 15:23:04 -0300

    push

--- index.py
+++ index.py
@@ -9,6 +9,14 @@ client = hazelcast.HazelcastClient(
         lambda state: print("Lifecycle event >>>", state),
     ]
 )
+# Get the Distributed Map from Cluster.
+my_map = client.get_map("my-distributed-map").blocking()
+# Standard Put and Get
+my_map.put("key", "value")
+my_map.get("key")
+# Concurrent Map methods, optimistic updating
+my_map.put_if_absent("somekey", "somevalue")
+my_map.replace_if_same("key", "value", "newvalue")
 
-print("Connected to cluster")
+
 client.shutdown()
